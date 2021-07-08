f70fa3cdcb8cdb27bad8fed71c3599eb9b6e675a
Breno Nunes <brenontavares@gmail.com>
2021-07-08 20:41:19 -0300

    feat: finishing Table component using data props

--- src/components/Table/index.js
+++ src/components/Table/index.js
@@ -1,14 +1,25 @@
-export function Table (props) {
-  console.log(props.data);
+const createTr = (arrayTd) => {
+  const trElement = document.createElement("tr");
 
+  arrayTd.forEach((td) => {
+    const tdElement = document.createElement("td");
+    tdElement.textContent = td;
+
+    trElement.appendChild(tdElement);
+  });
+
+  return trElement.outerHTML;
+};
+
+export function Table(props) {
   return `
     <table>
       <tr>
         <th>Nome</th>
+        <th>Média</th>
+        <th>Status</th>
       </tr>
-      <tr>
-        <td>Breno</td>
-      </tr>
+      ${props.data.map((dataItem) => createTr(Object.values(dataItem))).join()}
     </table>
-  `
+  `;
 }
--- src/index.js
+++ src/index.js
@@ -1,20 +1,20 @@
-import { Alunos } from "./_mock/Alunos.js"
-import "./css/reset.css"
-import "./css/app.css"
-import { Header } from "./components/Header/index.js"
-import estartandodevs from "./assets/estartandodevs.png"
-import { listaAlunosComMediaeStatus } from "./_mock/listaAlunosComMediaeStatus.js"
-import { Table } from "./components/Table/index.js"
+import { Alunos } from "./_mock/Alunos.js";
+import "./css/reset.css";
+import "./css/app.css";
+import { Header } from "./components/Header/index.js";
+import estartandodevs from "./assets/estartandodevs.png";
+import { listaAlunosComMediaeStatus } from "./_mock/listaAlunosComMediaeStatus.js";
+import { Table } from "./components/Table/index.js";
 
 const app = document.getElementById("app");
 
 const header = Header({
   class: "dark",
-  imgSrc: estartandodevs
-})
+  imgSrc: estartandodevs,
+});
 
-app.innerHTML = header
+app.innerHTML = header;
 
-const alunosComMedia = listaAlunosComMediaeStatus(Alunos)
+const alunosComMedia = listaAlunosComMediaeStatus(Alunos);
 
-app.innerHTML += Table({ data: alunosComMedia })
+app.innerHTML += Table({ data: alunosComMedia });
